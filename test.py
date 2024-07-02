23decf8588bcfb618092794075adeeeb81bcbea1
Namnguyen99999 <namnguyentien6.com>
2024-07-03 01:50:50 +0700

    them tep test.py

---
+++ test.py
@@ -0,0 +1,11 @@
+# Chuỗi thứ nhất
+string1 = "Hello"
+
+# Chuỗi thứ hai
+string2 = "World"
+
+# Nối hai chuỗi lại với nhau
+result = string1 + " " + string2
+
+# In kết quả ra màn hình
+print(result)
